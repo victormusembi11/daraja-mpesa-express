f4a63261d712d49ca74316b3e69cf00d919b2984
victormusembi11 <victor.musembi11@gmail.com>
2024-04-22 09:45:31 +0300

    ref: remove token str from path in token routes

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const TokenRoute = require("./routes/token");
 const app = express();
 
 app.listen(5000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 5000");
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use("/api/token", TokenRoute);
+app.use("/api", TokenRoute);
