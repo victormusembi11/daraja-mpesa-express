1c2366a7e63238917d1ef7b4cbc5e2c4ea8604d9
victormusembi11 <victor.musembi11@gmail.com>
2024-04-22 10:28:13 +0300

    feat: func to gen timestamp

---
+++ server/utils/timestamp.js
@@ -0,0 +1,13 @@
+const generateTimestamp = () => {
+  const date = new Date();
+  return (
+    date.getFullYear() +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    ("0" + date.getDate()).slice(-2) +
+    ("0" + date.getHours()).slice(-2) +
+    ("0" + date.getMinutes()).slice(-2) +
+    ("0" + date.getSeconds()).slice(-2)
+  );
+};
+
+module.exports = { generateTimestamp };
