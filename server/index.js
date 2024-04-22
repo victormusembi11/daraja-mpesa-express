5fe4d1df44a29fd320daf25fa4205656efcfdccd
victormusembi11 <victor.musembi11@gmail.com>
2024-04-22 15:19:58 +0300

    conf: cors headers config

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const TokenRoute = require("./routes/token");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
