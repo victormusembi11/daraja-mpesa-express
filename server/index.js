785db690004ab79daf184f9ba2fc64c70127bf7a
victormusembi11 <victor.musembi11@gmail.com>
2024-04-22 16:12:09 +0300

    ref: get server port as env var

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 
 const TokenRoute = require("./routes/token");
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
