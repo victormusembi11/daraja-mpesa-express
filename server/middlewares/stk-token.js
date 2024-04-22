85780b04800eb2dcffb7d480e54cdb5df8f1e6e5
victormusembi11 <victor.musembi11@gmail.com>
2024-04-22 10:30:18 +0300

    feat: fetch access token from saf sandbox for stk push

--- server/middlewares/stk-token.js
+++ server/middlewares/stk-token.js
@@ -1,3 +1,30 @@
-const createToken = (req, res, next) => {};
+const axios = require("axios");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const createToken = async (req, res, next) => {
+  const consumer = process.env.CONSUMER_KEY;
+  const secret = process.env.CONSUMER_SECRET;
+
+  try {
+    const response = await axios({
+      url: "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+      method: "GET",
+      headers: {
+        Authorization: `Basic ${Buffer.from(`${consumer}:${secret}`).toString(
+          "base64"
+        )}`,
+      },
+    });
+
+    req.token = response.data.access_token;
+    console.log(response.data);
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred");
+  }
+};
 
 module.exports = { createToken };
